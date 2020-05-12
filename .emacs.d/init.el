a6e3b909272fd12a1b6541827abae5447a370ede
Aditya Rathod <15635979+applecrazy@users.noreply.github.com>
2020-05-11 22:35:25 -0700

    .emacs.d: initial config commit

---
+++ .emacs.d/init.el
@@ -0,0 +1,27 @@
+;; Configure package.el to use MELPA and the org ELPA repository.
+(require 'package)
+
+(add-to-list 'package-archives
+	     '("org" . "https://orgmode.org/elpa/") t)
+(add-to-list 'package-archives
+             '("melpa" . "http://melpa.milkbox.net/packages/") t)
+(package-initialize)
+
+(require 'org)
+
+(org-babel-load-file
+ (expand-file-name "config.org" user-emacs-directory))
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(org-agenda-files (quote ("~/Dropbox/org/general.org")))
+ '(package-selected-packages (quote (org-bullets org snazzy-theme))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
